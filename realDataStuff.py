057f9e07f857d05fef582f369e75f1b33fba6ce4
frank <ffavela@gmail.com>
2016-05-26 19:45:49 -0500

    Pedro's loading data stuff functions

---
+++ realDataStuff.py
@@ -0,0 +1,25 @@
+import csv
+import pickle
+#Pedro's code
+
+def pickleRealData(txtFileName="data.txt", pklName="data.pkl"):
+    #new lists
+    aList,bList=[],[]
+    #open the txt file and put it into a list with strings
+    with open(txtFileName) as tsvfile:
+        tsvreader = csv.reader(tsvfile, delimiter="\t")
+        for line in tsvreader:
+            aList.append(line[:])
+    #convert the strings into integers
+    for e in aList:
+            bList.append([int(i) for i in e])
+    #save the shit out of the data in a new thing called pickle
+    fileObject=open(pklName,'wb')
+    pickle.dump(bList,fileObject)
+    fileObject.close()
+
+def unpickleRealData(pklName="data.pkl"):
+    pkl_file = open(pklName,'rb')
+    dataInList = pickle.load(pkl_file)
+    pkl_file.close()
+    return dataInList
