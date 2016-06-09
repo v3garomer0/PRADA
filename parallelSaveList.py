ff863cc5aa1c0a27cb5f49b657c46f9b725c6d63
frank <ffavela@gmail.com>
2016-06-09 10:45:13 -0500

    Parallelization works!!

--- parallelSaveList.py
+++ parallelSaveList.py
@@ -1,44 +1,56 @@
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Manager,cpu_count
 from PRADAfunctions import *
 
-def parallelPolMiniLex(sharedLexicon,theDict,theIndex):
-    tempMiniLex=getMiniLexicon(sharedLexicon,theIndex)
+def parallelPolMiniLex(lexicon,theList,theIndex):
+    tempMiniLex=getMiniLexicon(lexicon,theIndex+1)
     tempPolMiniLex=getPolygons4MiniLex(tempMiniLex)
     if tempPolMiniLex == {}:
         return
-    theDict[theIndex]=tempPolMiniLex
+    theList[theIndex]=tempPolMiniLex
 
 #for multiprossecing there has to be a main
 if __name__ == '__main__':
     manager = Manager()
-    myNProc=8
+    myNProc=cpu_count()
+    tDN=16 #total number of detectors
+    print ("Number of processors = ",myNProc)
 
-    polyDict = manager.list()
-    sharedLexicon = manager.dict()
+    polyList = manager.list()
+    for i in range(tDN):
+        polyList.append({})
     
     lexicon=openLexiconFile("lexicon100k.pkl")
-    #copy lexicon into sharedLexicon
-    for e in lexicon:
-        sharedLexicon[e]=lexicon[e]
-
+  
     #target is the function
-    processes = [Process(target=parallelPolMiniLex,args=(sharedLexicon,polyDict,i+1)) for i in range(2)]
+    processes = [Process(target=parallelPolMiniLex,
+                         args=(lexicon,polyList,i)) for i in range(tDN)]
     
     # start the processes
-    for p in processes:
-        p.start()
+    # for p in processes:
+    #     p.start()
         
     # Join them so everything is syncronized
-    for p in processes:
-        p.join()
-        
+    # for p in processes:
+    #     p.join()
+
+    for i in range(8):
+        processes[i].start()
+
+    for i in range(8):
+        processes[i].join()
+
+    for i in range(8,16):
+        processes[i].start()
+
+    for i in range(8,16):
+        processes[i].join()
+
     # for val in polyDict:... curiosly doesn't work
     # for i in range(1,myNProc):
     #     print (i+1, polyDict[i+1])
 
-    # file_name="polMiniLexDictParallel.pkl"
-    # fileObject=open(file_name,"wb")
-    # #Maybe copy polyDict into a simple dictionary
-    # pickle.dump(polyDict,fileObject)
-    # fileObject.close()
-    
+    file_name="polMiniLexListParallel.pkl"
+    fileObject=open(file_name,"wb")
+    #Maybe copy polyDict into a simple dictionary
+    pickle.dump(list(polyList),fileObject)
+    fileObject.close()
